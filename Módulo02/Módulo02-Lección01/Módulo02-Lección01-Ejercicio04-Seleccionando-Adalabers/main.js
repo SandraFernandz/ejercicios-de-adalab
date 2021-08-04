3f91226f918dbb6abaa6a9f7f2c41451fb53aa45
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-04 18:00:51 +0200

    working with adding variables and modifying them

---
+++ Módulo02/Módulo02-Lección01/Módulo02-Lección01-Ejercicio04-Seleccionando-Adalabers/main.js
@@ -0,0 +1,17 @@
+
+/*obtiene la primera etiqueta nombre*/ 
+const name1 = document.querySelector(".item1");
+
+/*obtiene la segunda etiqueta nombre */
+
+const name2 = document.querySelector(".item2");
+
+/* obtiene la otra etiqueta */
+const choice = document.querySelector(".title");
+
+/*modifica la etiqueta añadiendo e incluimos espacio en blanco
+choice.innerHTML = choice.innerHTML + " " + name1.innerHTML;*/
+
+/*modifica la etiqueta añadiendo e incluimos espacio en blanco*/
+choice.innerHTML = choice.innerHTML + " " + name2.innerHTML;
+
