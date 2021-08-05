bbcef81fb294c76b63f0ad803dd8314953ca9e2c
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-05 07:43:30 +0200

    working on exercise 5 lesson 1 module 2

---
+++ Módulo02/Módulo02-Lección01/Módulo02-Lección01-Ejercicio05-Progete-ese-Password/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/*obtenemos un elemento html */
+const passWord = document.querySelector(".password");
+
+/* lo modificamos */
+passWord.innerHTML = "**";
---
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio01-Trasteando-la-Consola/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const num1 = 7;
+const num2 = 5;
+const result = num1 + num2;
+const visible = document.querySelector(result).innerHTML;
