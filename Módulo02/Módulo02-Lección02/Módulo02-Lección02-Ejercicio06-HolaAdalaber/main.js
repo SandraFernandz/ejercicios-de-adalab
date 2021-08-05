649448e5af5c23ae8bb74c5680f6324fcf0526f9
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-05 18:25:46 +0200

    trying to solve exercise 06

---
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio06-HolaAdalaber/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const greetingAdalaber = "Lola";
+
+// document.querySelector(".js_adalaber").innerHTML = greetingAdalaber;
+
+// document.querySelector(".js_adalaber") = greetingAdalaber.innerHTML;
+
+
+const nameAdalaber = document.querySelector(".js_adalaber");
+
+nameAdalaber.innerHTML = "Lola";
+
---
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio07-InterpolaLola/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const greetingAdalaber = document.querySelector(".js-adalaber");
+const textToShow = "Lola";
+
+greetingAdalaber.innerHTML = ${textToShow};
+
+
+
+
+
+
+
