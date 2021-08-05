4d443c75a6a33120b6ab2ca06a81cb94ee1e21ae
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-05 16:14:38 +0200

    starting exercise 04, solved in console

---
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio03-Precio-Fruta/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const kiwi = 5;
+const pera = 2;
+const uva = 4;
+
+//const kiwiTotal = kiwi * 2;
+//const peraTotal = pera * 3;
+//const uvaTotal = uva * 0.5;
+
+const kiwi = document.querySelector(".js_kiwi");
+
