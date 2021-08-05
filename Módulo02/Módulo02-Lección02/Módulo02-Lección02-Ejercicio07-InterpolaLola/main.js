c76aaee2d3e65a978c7573ee7dd29d8dd9f6dc36
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-05 19:43:40 +0200

    playing with interpolacion de cadenas

--- Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio07-InterpolaLola/main.js
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio07-InterpolaLola/main.js
@@ -1,11 +1,16 @@
 "use strict";
 
-const greetingAdalaber = document.querySelector(".js-adalaber");
-const textToShow = "Lola";
+// const greetingAdalaber = document.querySelector(".js-adalaber");
+// const textToShow = "Lola";
 
-greetingAdalaber.innerHTML = ${textToShow};
+// greetingAdalaber.innerHTML = ${textToShow};
 
 
+//en la consola escribiríamos//
+const adalaber = "Lola";
+//es importante que las comillas de la frase sean acentos abiertos para hacer que la constante no aparezca como "literal" sino interpretada como variable que hay que substituir
+const greeting =`Hola ${adalaber} como estás`;
+console.log(greeting);
 
 
 
