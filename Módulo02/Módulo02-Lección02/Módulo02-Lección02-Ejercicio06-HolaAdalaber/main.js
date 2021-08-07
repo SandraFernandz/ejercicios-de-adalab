2d838d658645a1e677800c80f25a0fe80cd162b9
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-07 15:28:46 +0200

    commit

--- Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio06-HolaAdalaber/main.js
+++ Módulo02/Módulo02-Lección02/Módulo02-Lección02-Ejercicio06-HolaAdalaber/main.js
@@ -6,12 +6,23 @@
 
 // document.querySelector(".js_adalaber") = greetingAdalaber.innerHTML;
 
-
-const nameAdalaber = document.querySelector(".js_adalaber");
-nameAdalaber.innerHTML = "Lola";
-
+const adalaber = document.querySelector(".js_adalaber");
 const greeting = document.querySelector(".js_greeting");
+const delighted = document.querySelector(".js_delighted");
+
 greeting.innerHTML = "Hola";
+adalaber.innerHTML = "Lola";
+delighted.innerHTML = "encantada de conocerte";
+
+console.log( `${greeting} + " " + ${adalaber}+ "," + ${delighted}`); 
+
+
+
+// const nameAdalaber = document.querySelector(".js_adalaber");
+// nameAdalaber.innerHTML;
+
+// const greeting = document.querySelector(".js_greeting");
+// greeting.innerHTML = "Hola";
 
 
 
