6dfeca1119602137f8388e4694fbdbd7898186a3
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-09 11:07:34 +0200

    solving exercises

---
+++ Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio04-calculadoraPerros/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let dogAge = 0;
+
+if (dogAge === 1){
+    console.log("El primer año de un perro equivale a 15 años de humano");
+} else if (dogAge === 2){
+    console.log("El segundo año de un perro equivale a nueve años de humano");
+} else if(dogAge < 1){
+    console.log("Por favor, ingresa un número correcto");
+
+} else {
+    console.log ("A partir del tercero, cada año de perro equivale a 5 años de humano");
+}
