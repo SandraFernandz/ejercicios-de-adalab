3897f04a4c0dc4d25880653bc9f5cb832513d5b0
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-09 10:50:39 +0200

    resolving exercise 1

--- Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio01-NadieSinAvatar/main.js
+++ Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio01-NadieSinAvatar/main.js
@@ -3,7 +3,10 @@
 // avatar por defecto
 const DEFAULT_AVATAR = 'http://placehold.it/300x300';
 // avatar que eligió el usuario al registrarse
-let userAvatar = 'http://www.fillmurray.com/300/300';
+let userAvatar = '';
 
-const imageAvatar=document.querySelector(".js_image");
-imageAvatar.innerHTML = userAvatar;
+// const imageAvatar=document.querySelector(".js_image");
+
+let newAvatar = document.querySelector(".js_image");
+newAvatar.src = userAvatar || DEFAULT_AVATAR;
+
