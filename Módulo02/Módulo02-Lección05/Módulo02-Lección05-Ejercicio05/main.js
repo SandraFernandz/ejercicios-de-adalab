da8c4dea4a493c4634d3e026184bd8f916ccbbee
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-11 11:18:37 +0200

    adding explanations to exercise

--- Módulo02/Módulo02-Lección05/Módulo02-Lección05-Ejercicio05/main.js
+++ Módulo02/Módulo02-Lección05/Módulo02-Lección05-Ejercicio05/main.js
@@ -3,6 +3,8 @@ const bodyEl = document.querySelector("body");
 
 function changeColor (event){
     console.log(event.code);
+    
+//las clases .red y .purple se crean como estilos en css pero no se le da la clase al elemento al que se van a aplicar. se les añade el estilo con js. pero no al html
 
     if(event.code === "KeyR"){
         bodyEl.classList.add("red")} else if(event.code ==="KeyM"){
