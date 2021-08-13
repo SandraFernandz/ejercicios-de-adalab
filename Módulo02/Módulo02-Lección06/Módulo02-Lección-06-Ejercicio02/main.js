76aeac5304e34c33e4da65bd386e9ae9b47528d5
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-13 16:52:08 +0200

    changing a text with js innerHTML

---
+++ Módulo02/Módulo02-Lección06/Módulo02-Lección-06-Ejercicio02/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const message = document.querySelector(".js_warning");
+message.innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente';
