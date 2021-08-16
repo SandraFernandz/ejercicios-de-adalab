371493db35a1685c21f9b0876ac78047c0f51ae0
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-16 09:00:03 +0200

    practicing functions

---
+++ Módulo02/Módulo02-Lección04/Módulo02-Lección04-Ejercicio01/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//en cuanto tenemos más de una línea de código necesitamos usar paréntesis para la función arrow. además, la función arrow con paréntesis siempre espera return para efectuarse.
+
+const multFunction = (a, b) =>{
+    return a * b
+}
+
+console.log(multFunction(6,9));
