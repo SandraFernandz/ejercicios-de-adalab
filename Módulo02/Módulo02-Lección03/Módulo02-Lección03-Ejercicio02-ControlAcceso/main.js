e513d1d8acec5e6c8746c4b37b93d4e1aff42381
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-18 10:38:11 +0200

    solving exercise 2 lesson 3

--- Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio02-ControlAcceso/main.js
+++ Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio02-ControlAcceso/main.js
@@ -1,15 +1,24 @@
 "use strict";
 
-const welcomMessage = document.querySelector(".js_message");
-welcomMessage.innerHTML = "Bienvenida";
-const moreThan = "tienes más de 30 años";
-const lessThan = "tienes menos de 30 años";
+// const welcomMessage = document.querySelector(".js_message");
+// welcomMessage.innerHTML = "Bienvenida";
+// const moreThan = "tienes más de 30 años";
+// const lessThan = "tienes menos de 30 años";
 
-const adalaber = 26;
+// const adalaber = 26;
 
-if (adalaber < 31){
-    console.log('${welcomeMessage} ${lessThan}');
-} else {
-    console.log('${welcomeMessage} ${moreThan}');
+// if (adalaber < 31){
+//     console.log('${welcomeMessage} ${lessThan}');
+// } else {
+//     console.log('${welcomeMessage} ${moreThan}');
+// }
+
+
+let userName = 'Clara';
+
+if (userName === 'Clara' || 'Ana') {
+    console.log(`Bienvenida! ${userName}`);
 }
-
+else {
+    console.log('Lo siento pero el usuario que has introducido no está registrado');
+}
