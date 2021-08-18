6b62b7333bdd5731d659c8ce3288b9a0383ebd78
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-18 11:15:50 +0200

    solving exercise 08

--- Módulo02/Módulo02-Lección05/Módulo02-Lección05-Ejercicio08-MásBotones/main.js
+++ Módulo02/Módulo02-Lección05/Módulo02-Lección05-Ejercicio08-MásBotones/main.js
@@ -1,11 +1,30 @@
 'use strict';
 
-const button = document.querySelector('.js_button');
+const button1 = document.querySelector('.js_button1');
+const button2 = document.querySelector('.js_button2');
 
-function chgBackground(){
+
+function chgBackground(event){
+
+    if(event.target === button1){
+        button1.classList.toggle('buttonStyle');
+    } else if(event.target === button2){
+        button2.classList.toggle('buttonStyle');
+    }  
    
-    button.classList.toggle('buttonStyle');
 }
 
+button1.addEventListener('click', chgBackground);
+button2.addEventListener('click', chgBackground);
 
-button.addEventListener('click', chgBackground);
+//otra manera de hacerlo es como lo ha hecho andrea
+// const button1 = document.querySelector('.button1');
+// const button2 = document.querySelector('.button2');
+
+// function changeStyle (event){
+//    const selectedButton = event.currentTarget;
+//     selectedButton.classList.toggle('buttonStyle');
+// }
+
+// button1.addEventListener('click', changeStyle);
+// button2.addEventListener('click', changeStyle);
