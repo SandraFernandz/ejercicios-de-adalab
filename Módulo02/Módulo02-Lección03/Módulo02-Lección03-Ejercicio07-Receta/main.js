4dd52de618c30c39f3cb57f0b67de7c88998c597
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-18 10:45:45 +0200

    solving exercise

--- Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio07-Receta/main.js
+++ Módulo02/Módulo02-Lección03/Módulo02-Lección03-Ejercicio07-Receta/main.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const ingredient = document.querySelector(".js_ingredient");
-const recipe = document.querySelector(".js_recipe");
+// const ingredient = document.querySelector(".js_ingredient");
+// const recipe = document.querySelector(".js_recipe");
 
-const todaysIngredient = ingredient.innerHTML;
+// const todaysIngredient = ingredient.innerHTML;
 
-debugger
-if (todaysIngredient === "Tomate"){
-    recipe.innerHTML = "Haz ensalada";
-} else{
-    recipe.innerHTML = `Hoy no comes`;
+// debugger
+// if (todaysIngredient === "Tomate"){
+//     recipe.innerHTML = "Haz ensalada";
+// } else{
+//     recipe.innerHTML = `Hoy no comes`;
+// }
+
+const userIngredient = 'tomate';
+
+switch (userIngredient) {
+    case 'plátano':
+        console.log('Puedes hacer un bizcocho');
+        break;
+    case 'tomate':
+    console.log('Puedes hacer gazpacho');
+        break;
+    case 'mozzarella':
+    console.log('Puedes hacer una pizza');
+        break;
+    default:
+        console.log('Si no tienes plátano, tomate ni mozzarella puedes comer fuera');
 }
