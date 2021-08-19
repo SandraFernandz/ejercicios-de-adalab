7be02828afc860fab27b8216ff5079b9aeaba217
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-19 08:58:09 +0200

    solving exercise 02 lesson 06

--- Módulo02/Módulo02-Lección06/Pruebas/Ejercicio02-A-laCarrera/main.js
+++ Módulo02/Módulo02-Lección06/Pruebas/Ejercicio02-A-laCarrera/main.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const adalaber1 = {
-    nombre: 'Susana',
-    edad: 34,
-    profesión: 'periodista',
-    run: function(){
-        return 'Estoy corriendo';
-    },
-    runAMarathon: function(){
-        const distance = 3;
-        console.log(`${run} una marathon de ${this.runAMarathon} kilometros`)
-    }
-}
+const adalaber1 = {};
+adalaber1.name = 'María';
+adalaber1.run = doing => `Estoy ${doing}`;
+
+//le pasamos un argumento (corriendo) al método (función) del objeto adalaber1
+console.log(adalaber1.run('corriendo'));
+
+//añadimos un nuevo método (runAMarathon) al objeto adalaber1, que toma un parámetro distance
+
+adalaber1.runAMarathon = distance => `una marathon de ${distance} kilometros`;
+
+console.log(adalaber1.runAMarathon(50));
+console.log(adalaber1.run('corriendo') + " " + adalaber1.runAMarathon('50'));
