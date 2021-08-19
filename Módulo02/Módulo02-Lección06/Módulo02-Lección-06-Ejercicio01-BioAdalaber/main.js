a7a6d668ee0584a9b00f492a3a42c68ebc5a40d0
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-19 09:06:21 +0200

    adding options to exercise 01

--- Módulo02/Módulo02-Lección06/Módulo02-Lección-06-Ejercicio01-BioAdalaber/main.js
+++ Módulo02/Módulo02-Lección06/Módulo02-Lección-06-Ejercicio01-BioAdalaber/main.js
@@ -9,3 +9,6 @@ const mensaje = document.querySelector(".js_mensaje");
 
 
 mensaje.innerHTML = "Hola me llamo" + adalaber1.nombre;
+
+// otra opción
+//mensaje.innerHTML = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} años y soy ${adalaber1.profession}`;
