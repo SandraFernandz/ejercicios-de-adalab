906a97805c4e6bac6eee6623b41aea53d23eb148
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-19 09:03:16 +0200

    solving exercise 02

--- Módulo02/Módulo02-Lección06/Módulo02-Lección-06-Ejercicio02-A-laCarrera/main.js
+++ Módulo02/Módulo02-Lección06/Módulo02-Lección-06-Ejercicio02-A-laCarrera/main.js
@@ -1,4 +1,15 @@
 'use strict';
 
-const message = document.querySelector(".js_warning");
-message.innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente';
+const adalaber1 = {};
+adalaber1.name = 'María';
+adalaber1.run = doing => `Estoy ${doing}`;
+
+//le pasamos un argumento (corriendo) al método (función) del objeto adalaber1
+console.log(adalaber1.run('corriendo'));
+
+//añadimos un nuevo método (runAMarathon) al objeto adalaber1, que toma un parámetro distance
+
+adalaber1.runAMarathon = distance => `una marathon de ${distance} kilometros`;
+
+console.log(adalaber1.runAMarathon(50));
+console.log(adalaber1.run('corriendo') + " " + adalaber1.runAMarathon('50'));
