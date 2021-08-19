951b4fdc44040f19f53506d02d9931e0afae4372
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-19 13:42:29 +0200

    adding quotes

--- Módulo02/Módulo02-Lección06/Módulo02-Lección06-Ejercicio07-EstructuraDatosUsusario/main.js
+++ Módulo02/Módulo02-Lección06/Módulo02-Lección06-Ejercicio07-EstructuraDatosUsusario/main.js
@@ -8,6 +8,7 @@ user.lastName = 'Pérez';
 user.age = 45;
 user.job = job;
 
+//primer console.log
 console.log(user);
 
 user.firstName = 'Andrés';
@@ -15,4 +16,5 @@ user.age = user.age + 1;
 
 //en el primer console.log se retoman los cambios efectuados a posteriori pero además muestra valores iniciales;
 
+//segundo console.log con valores reasignados
 console.log(user);
