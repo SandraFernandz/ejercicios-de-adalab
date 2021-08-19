44d6c06e1377b031ca81460f26ba2dcb60e5049f
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-19 08:24:03 +0200

    solving part of exercise 1

---
+++ Módulo02/Módulo02-Lección06/Pruebas/Ejercicio01-BioAdalaber/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const adalaber1 = {
+    nombre: 'Susana',
+    edad: 34,
+    profesión: 'periodista'
+}
+
+console.log(`Mi nombre es ${adalaber1.nombre} tengo ${adalaber1.edad} años y soy ${adalaber1.profesión}`);
