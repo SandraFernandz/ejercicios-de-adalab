0020f5e14f44e242591d6fccb440d64873becc61
Sandra Fernandz <sandraenmunich@gmail.com>
2021-08-29 13:09:47 +0200

    adding practice midterm exercise

--- Módulo02/Módulo02-Ejercicio-Intermedio/main.js
+++ Módulo02/Módulo02-Ejercicio-Intermedio/main.js
@@ -1,16 +1,47 @@
 'use strict';
 
+//variables
 const chooseMood = document.querySelector('.js_mood');
-const moodButton = document.querySelector('.js_button');
+const updateButton = document.querySelector('.js_button');
 const mainFace = document.querySelector('.js_face');
+const happyFace = document.querySelector('.js_happy');
+const unhappyFace = document.querySelector('.js_unhappy');
 
-function changeMood(){
-  if(moodButton === ':)'){
-        mainFace.innerHTML = ':)'
-  } else{
-        mainFace.innerHTML = ':('
-  }
+//muy importante crear esta variable para usarla después en relación con classList.add y classList.remove
+const body = document.querySelector('.body');
+
+// función para cambiar de carita feliz a carita triste pero he optado por una manera más sencilla
+
+// function changeMood(){
+//   if(chooseMood.value === 'happy'){
+//         mainFace.innerHTML = ':)'
+//   } else if(chooseMood.value === 'unhappy'){
+//         mainFace.innerHTML = ':('
+//   }
+// }
+
+// updateButton.addEventListener('click', changeMood);
+
+//función que cambia el color de fondo. muy imp crear const body con querySelector para usar en classList.add/remove
+function createRandomNum (){
+      //cambiar carita centro según botón input
+      //pregunta: porqué no funciona con chooseMood.innerHTML en lugar de chooseMood.value?
+      mainFace.innerHTML = chooseMood.value;
+
+      let randomNum = Math.floor(Math.random() * 101);
+      if(randomNum %2 !== 0){
+            body.classList.remove('happy_body');
+            body.classList.add('unhappy_body');
+      }else{
+            body.classList.remove('unhappy_body');
+            body.classList.add('happy_body');
+    
+      }
+      console.log(randomNum);
 }
+// function getRandomInt(max){
+//       return Math.floor(Math.random() * max);
+// }
+updateButton.addEventListener('click', createRandomNum);
 
 
-moodButton.addEventListener('click', changeMood);
