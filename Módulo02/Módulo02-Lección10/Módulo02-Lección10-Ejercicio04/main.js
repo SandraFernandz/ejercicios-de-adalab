3fe6748effcff1817304d1a5146b3480f4f79cb0
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-03 12:42:25 +0200

    adding exercises

--- Módulo02/Módulo02-Lección10/Módulo02-Lección10-Ejercicio04/main.js
+++ Módulo02/Módulo02-Lección10/Módulo02-Lección10-Ejercicio04/main.js
@@ -1,13 +1,18 @@
-const button = document.querySelector('.js_button');
-const input = document.querySelector('.js_input');
+'use strict';
 
-// function getInfoHandler(){
-//     fetch('https://api.github.com/orgs/Adalab')
-//     .then (response => response.json())
-//      .then(data => ){
+const searchUsername = document.querySelector('.js-btn');
+const result = document.querySelector('.js-name');
+const input = document.querySelector('.js-username');
+const photo = document.querySelector('.js-img');
+const repos = document.querySelector('.js-repo');
 
-//      }
-// }
+function handleGetUser() {
+    fetch(`https://api.github.com/users/${input.value}`)
+    .then(response => response.json())
+    .then(data => { 
+        photo.src = data.avatar_url;
+        repos.innerHTML += data.public_repos;
+    });
+}
 
-
-// button.addEventListener('click', getInfoHandler);
+searchUsername.addEventListener('click', handleGetUser);
