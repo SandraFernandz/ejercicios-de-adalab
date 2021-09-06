dd8e13634951c5b97872693e11f4d1f44b1a2f99
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-06 08:23:10 +0200

    adding practical exercises

---
+++ Módulo02/Módulo02-Lección11/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const input = document.querySelector(".js_input");
+const button = document.querySelector("js_button");
+
+function provideNamesHandle() {}
+//  fetch("https://swapi.dev/people/")
+//      .then(response => response.json())
+//      .then(nameOfCharacers =>{
+//              console.log(nameOfCharacters);
+//          }
+//              ){
+//          }
+//      )
+
+button.addEventListener("click", provideNamesHandle);
