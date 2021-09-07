bc1daad9286c115797f8847f40308f24a59a1a44
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-07 07:41:37 +0200

    solving exercise

--- Módulo02/Módulo02-Lección11/main.js
+++ Módulo02/Módulo02-Lección11/main.js
@@ -1,13 +1,20 @@
 "use strict";
 
 const input = document.querySelector(".js_input");
-const button = document.querySelector("js_button");
+const button = document.querySelector(".js_button");
 
 function provideNamesHandle() {}
-//  fetch("https://swapi.dev/people/")
-//      .then(response => response.json())
-//      .then(nameOfCharacers =>{
-//              console.log(nameOfCharacters);
+let character = document.querySelector(".js_input").value;
+fetch(`https://swapi.dev/api/people/?search=${character}`)
+  .then((response) => response.json())
+  .then((data) => {
+    let result = document.querySelector(".js_result");
+
+    for (const pepino of data.results) {
+      result.innerHTML += `<li>Personaje: ${pepino.name}, Género: ${pepino.gender}</li>`;
+    }
+  });
+//              console.log(data);
 //          }
 //              ){
 //          }
