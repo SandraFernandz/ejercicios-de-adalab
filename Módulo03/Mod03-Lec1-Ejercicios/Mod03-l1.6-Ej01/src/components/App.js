4bf6ab5523bff9718b8ead5345454bd9f50cd22e
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-17 06:52:49 +0200

    adding git ignore

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-l1.6-Ej01/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-l1.6-Ej01/src/components/App.js
@@ -3,7 +3,45 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      {/* <h1>Hola Mundo</h1> */}
+      <header className="header">
+        <div className="image">
+          <a href="https://adalab.es/" title="Logo de Adalab">
+            <img
+              src="./images/adalab-logo-155x61.png"
+              title="Logo de Adalab"
+              alt="Logo Adalab"
+            />
+          </a>
+        </div>
+        <nav>
+          <ul className="nav">
+            <li>
+              <a
+                href="https://adalab.es/blog/"
+                className="item"
+                title="Nuestro Blog"
+              >
+                Blog
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://adalab.es/contacto/"
+                className="item"
+                title="Contacta con nosotros"
+              >
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="main">
+        <h1 className="message">Creando diversidad digital</h1>
+      </main>
     </div>
   );
 }
