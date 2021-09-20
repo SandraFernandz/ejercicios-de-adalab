05ceb0344ad3ac9072588efc8fc06ee499f54a06
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-20 15:15:14 +0200

    commit

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-L2.1-Contador/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-L2.1-Contador/src/components/App.js
@@ -1,5 +1,4 @@
 import '../styles/App.scss';
-
 import { useState } from 'react';
 
 function App() {
@@ -17,3 +16,19 @@ function App() {
 }
 
 export default App;
+
+// import { useState } from 'react';
+// function App() {
+//   const [number, setNumber] = useState(0);
+//   const handleCounter = () => {
+//     setNumber(number + 1);
+//   };
+//   return (
+//     <div>
+//       <h1>Contador:{number} </h1>
+//       <p>Sube el número del contador haciendo 'click' en el botón</p>
+//       <button onClick={handleCounter}>Clickeame</button>
+//     </div>
+//   );
+// }
+// export default App;
