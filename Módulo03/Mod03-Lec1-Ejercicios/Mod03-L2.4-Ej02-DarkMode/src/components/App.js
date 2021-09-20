d9621d51e9e78778807242dbb3e0d0891d112675
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-20 11:16:26 +0200

    adding exercise

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-L2.4-Ej02-DarkMode/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-L2.4-Ej02-DarkMode/src/components/App.js
@@ -1,17 +1,63 @@
 import '../styles/main.scss';
 import { useState } from 'react';
 
+// function App() {
+//   const [color, setColor] = useState('dark-mode');
+//   const [darkMode, setDarkMode] = useState(true);
+//   //const color = 'dark-mode'
+//   const handleButton = () => {
+//     if (!color) {
+//       setColor('dark-mode');
+//     } else {
+//       setColor('');
+//     }
+
+//     setDarkMode(!darkMode);
+//   };
+//   return (
+//     <div>
+//       <button onClick={handleButton}>Des/activar el dark mode</button>
+//       <p> Tienes activado el dark mode</p>
+//       <p className={color}>
+//         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et veniam cum
+//         eaque iste doloremque natus maiores qui odit at fugit praesentium earum
+//         culpa delectus, adipisci, illum maxime quia veritatis porro?
+//       </p>
+//       <p className={darkMode === true ? 'dark-mode' : ''}>
+//         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et veniam cum
+//         eaque iste doloremque natus maiores qui odit at fugit praesentium earum
+//         culpa delectus, adipisci, illum maxime quia veritatis porro?
+//       </p>
+//     </div>
+//   );
+// }
+// export default App;
+
+/*
+
+darkMode === true ? 'dark-mode' : ''
+
+if (darkMode === true) {
+  className = 'dark-mode'
+} else {
+  className = ''
+}
+
+*/
+
+//////////////////// ángela///////////////
+// import '../styles/main.scss';
+// import { useState } from 'react';
 function App() {
   const [color, setColor] = useState('dark-mode');
   const [darkMode, setDarkMode] = useState(true);
-  //const color = 'dark-mode'
+  //const color = ‘dark-mode’
   const handleButton = () => {
-    if (!color) {
+    if (color === '') {
       setColor('dark-mode');
     } else {
       setColor('');
     }
-
     setDarkMode(!darkMode);
   };
   return (
@@ -32,15 +78,11 @@ function App() {
   );
 }
 export default App;
-
 /*
-
-darkMode === true ? 'dark-mode' : ''
-
+darkMode === true ? ‘dark-mode’ : ‘’
 if (darkMode === true) {
-  className = 'dark-mode'
+  className = ‘dark-mode’
 } else {
-  className = ''
+  className = ‘’
 }
-
 */
