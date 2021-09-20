463b9ded5f22656fae30d46c3d36801de6a0952a
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-20 18:37:47 +0200

    adding exercise 1

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-L03.1-Ej01Eres Bot-o-Persona/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-L03.1-Ej01Eres Bot-o-Persona/src/components/App.js
@@ -2,61 +2,34 @@ import '../styles/App.scss';
 import { useState } from 'react';
 
 function App() {
-  const [ingredients, setIngredients] = useState(false);
+  const [ingredients, setIngredients] = useState('');
 
   const handleIngredients = (ev) => {
-    setIngredients(ev.target.checked);
-    if (ingredients === true) {
-      onChange{ingredients}
+    const inputValue = ev.target.id;
+    setIngredients(inputValue);
+    console.log(ingredients);
+
+    if (inputValue === ('patatas' && 'huevos' && 'cebolla')) {
+      console.log('Eres una persona cebollista');
+    } else {
+      console.log('Eres un robot sin paladar');
     }
   };
 
-  console.log(ingredients);
   return (
     <div>
       <h1>Selecciona los ingredientes de una tortilla de patatas</h1>
-      <form action="">
+      <form action="" onChange={handleIngredients}>
         <label htmlFor="macarrones">Macarrones</label>
-        <input
-          type="checkbox"
-          name="macarrones"
-          id="macarrones"
-          checked={ingredients}
-          onChange={handleIngredients}
-        />
+        <input type="checkbox" name="macarrones" id="macarrones" />
         <label htmlFor="patatas">Patatas</label>
-        <input
-          type="checkbox"
-          name="patatas"
-          id="patatas"
-          checked={ingredients}
-          onChange={handleIngredients}
-        />
+        <input type="checkbox" name="patatas" id="patatas" />
         <label htmlFor="nueces">Nueces</label>
-        <input
-          type="checkbox"
-          name="nueces"
-          id="nueces"
-          checked={ingredients}
-          onChange={handleIngredients}
-        />
+        <input type="checkbox" name="nueces" id="nueces" />
         <label htmlFor="huevos">Huevos</label>
-        <input
-          type="checkbox"
-          name="huevos"
-          id="huevos"
-          checked={ingredients}
-          onChange={handleIngredients}
-        />
+        <input type="checkbox" name="huevos" id="huevos" />
         <label htmlFor="cebolla">Cebolla</label>
-        <input
-          type="checkbox"
-          name="cebolla"
-          id="cebolla"
-          checked={ingredients}
-          onChange={handleIngredients}
-        />
-        <p>Los ingredientes necesarios son{ingredients}</p>
+        <input type="checkbox" name="cebolla" id="cebolla" />
       </form>
     </div>
   );
