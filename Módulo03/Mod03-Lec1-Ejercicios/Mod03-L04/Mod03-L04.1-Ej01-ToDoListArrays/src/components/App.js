9a1ad1439a6da0924cabb6b2513a1cf7eccfc056
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-21 16:18:50 +0200

    painting arrays

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-L04/Mod03-L04.1-Ej01-ToDoListArrays/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-L04/Mod03-L04.1-Ej01-ToDoListArrays/src/components/App.js
@@ -1,11 +1,25 @@
 import '../styles/App.scss';
 
-function App() {
+const App = () => {
+  const tasks = [
+    'Comprar harina, jamón y pan rallado',
+    'Hacer croquetas ricas',
+    'Ir a la puerta de un gimnasio',
+    'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+  ];
+
+  const renderTasks = () => {
+    return tasks.map((task, index) => {
+      return <li key={index}>Tengo que {task}</li>;
+    });
+  };
+
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1>Lista de To Do´s</h1>
+      <ul>{renderTasks()}</ul>
     </div>
   );
-}
+};
 
 export default App;
