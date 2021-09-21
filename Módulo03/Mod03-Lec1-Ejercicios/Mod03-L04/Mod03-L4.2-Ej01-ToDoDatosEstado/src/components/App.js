a1a50d8a8cdf5fcd81cb27c50fba3acc242a476d
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-21 17:43:42 +0200

    commit

--- Módulo03/Mod03-Lec1-Ejercicios/Mod03-L04/Mod03-L4.2-Ej01-ToDoDatosEstado/src/components/App.js
+++ Módulo03/Mod03-Lec1-Ejercicios/Mod03-L04/Mod03-L4.2-Ej01-ToDoDatosEstado/src/components/App.js
@@ -1,11 +1,35 @@
 import '../styles/App.scss';
 
-function App() {
+const App = () => {
+  const tasks = [
+    { task: 'Comprar harina, jamón y pan rallado', completed: true },
+    { task: 'Hacer croquetas ricas', completed: true },
+    { task: 'Ir a la puerta de un gimnasio', completed: false },
+    {
+      task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+      completed: false,
+    },
+  ];
+  //tarea representa cada elemento del array que tiene las propiedades task y completed, y podría ser pepino
+  const renderTasks = () => {
+    return tasks.map((tarea, index) => {
+      //faltaría tachar la lista con método de string
+      // if (tarea.completed === true) {
+      //   //función original String.strike();
+      //   tasks.task.strike();
+      // }
+
+      console.log(tarea);
+      return <li key={index}>Tengo que {tarea.task}</li>;
+    });
+  };
+
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1>Lista de To Do´s</h1>
+      <ul>{renderTasks()}</ul>
     </div>
   );
-}
+};
 
 export default App;
